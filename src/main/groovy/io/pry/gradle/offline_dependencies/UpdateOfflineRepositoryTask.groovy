942c747c75d68bb23277bd09759ab33c98bb3604
kov-serg <kovserg_rambler@rambler.ru>
2026-04-30 14:08:12 +0300

    Update UpdateOfflineRepositoryTask.groovy
    make gradle8 compatible

--- src/main/groovy/io/pry/gradle/offline_dependencies/UpdateOfflineRepositoryTask.groovy
+++ src/main/groovy/io/pry/gradle/offline_dependencies/UpdateOfflineRepositoryTask.groovy
@@ -17,6 +17,7 @@ import org.gradle.api.artifacts.component.ModuleComponentIdentifier
 import org.gradle.api.artifacts.result.UnresolvedArtifactResult
 import org.gradle.api.internal.artifacts.DefaultModuleIdentifier
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 import org.gradle.internal.component.external.model.DefaultModuleComponentIdentifier
 import org.gradle.ivy.IvyDescriptorArtifact
@@ -26,12 +27,11 @@ import org.gradle.language.base.artifact.SourcesArtifact
 import org.gradle.language.java.artifact.JavadocArtifact
 import org.gradle.maven.MavenModule
 import org.gradle.maven.MavenPomArtifact
-import org.gradle.util.GFileUtils
 
 import static io.pry.gradle.offline_dependencies.Utils.addToMultimap
 
 class UpdateOfflineRepositoryTask extends DefaultTask {
-
+    @Internal        
     def EMPTY_DEPENDENCIES_ARRAY = new Dependency[0]
 
     @Input
@@ -57,11 +57,11 @@ class UpdateOfflineRepositoryTask extends DefaultTask {
             // copy collected files to destination directory
             repositoryFiles.each { id, files ->
                 def directory = moduleDirectory(id)
-                GFileUtils.mkdirs(directory)
-                files.each { File file -> GFileUtils.copyFile(file, new File(directory, file.name)) }
+                project.mkdir(directory)                
+                files.each { File file -> project.copy { from file into directory rename { file.name } } }
             }
         }
-    }
+    
 
     // configurations
     private Set<Configuration> getConfigurations() {
